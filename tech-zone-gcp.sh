6ea6cde4706e5e6c1140d783ea3cd2fe2a0c7db8
AungSoePy1 <96154849+AungSoePy1@users.noreply.github.com>
2023-09-14 09:14:16 +0630

    Create tech-zone-gcp.sh

---
+++ tech-zone-gcp.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+plain='\033[0m'
+
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+# Font Size
+# h6 = SMALL , h4 = AVERAGE , h3 = LARGE
+# Check Root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
+
+sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
+echo "
+<h3><font color='red'>▬▬▬▬▬▬۩۞۩▬▬▬▬▬▬
+</font></h3>
+<h3><font color='cyan'>------ ۩ PREMIUM SERVER ۩ ------
+</font></h3>
+
+<h3><font color='cyan'>--- ۩ SERVER BY TECG ZONE 🇲🇲 ۩ ---
+</font></h3>
+
+<h4><font color='cyan'>--- ۩ GCP SINHAPORE SERVER 🇸🇬 ۩ ---
+</font></h4>
+
+<h4><font color='cyan'>------ ۩ THANL YOU ۩ ------
+</font></h4>
+
+<h3><font color='red'>▬▬▬▬▬▬۩۞۩▬▬▬▬▬▬
+</font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
+useradd "zero" --shell=/bin/false -M
+echo "zero:zero" | chpasswd
+
+echo ""
+
+echo -e "${red}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+echo -e "${cyan}  /00000000 /00000000 /0000000   /000000                                      ${plain}"
+echo -e "${cyan} |_____ 00 | 00_____/| 00__  00 /00__  00                                     ${plain}"
+echo -e "${cyan}      /00/ | 00      | 00  \ 00| 00  \ 00                                     ${plain}"
+echo -e "${cyan}     /00/  | 00000   | 0000000/| 00  | 00                                     ${plain}"
+echo -e "${cyan}    /00/   | 00__/   | 00__  00| 00  | 00                                     ${plain}"
+echo -e "${cyan}   /00/    | 00      | 00  \ 00| 00  | 00                                     ${plain}"
+echo -e "${cyan}  /00000000| 00000000| 00  | 00|  000000/                                     ${plain}"
+echo -e "${cyan} |________/|________/|__/  |__/ \______/                                      ${plain}"
+echo -e "${green}Contact the developer https://t.me/Nora7684 for more information             ${plain}"
+echo -e "${red}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+
+
+echo -e "${cyan}I AM ZERO🇲🇲 ${plain}"
